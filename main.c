b0263272ab636a7a803dfb0b8841f233febba1b1
jb <101614021+jbc2313@users.noreply.github.com>
2023-06-28 02:01:29 -0500

    update
    added browser object

--- main.c
+++ main.c
@@ -5,12 +5,14 @@ int main(int argc, char **argv)
     FL_FORM *form;
     FL_OBJECT *button_one;
     FL_OBJECT *input_one;
+    FL_OBJECT *browser_one;
 
     fl_initialize(&argc, argv, 0, 0, 0);
 
-    form = fl_bgn_form(FL_UP_BOX, 440, 200);
+    form = fl_bgn_form(FL_UP_BOX, 640, 400);
     button_one = fl_add_button(FL_NORMAL_BUTTON, 20, 20, 110, 40, "Enter the Matrix");
-    input_one = fl_add_input(FL_NORMAL_INPUT, 70, 110, 90, 40, "Filename");
+    input_one = fl_add_input(FL_NORMAL_INPUT, 210, 20, 90, 40, "Filename");
+    browser_one = fl_add_browser(FL_MULTI_BROWSER, 20, 110, 200, 200, "Filesystem");
     fl_end_form();
 
 
